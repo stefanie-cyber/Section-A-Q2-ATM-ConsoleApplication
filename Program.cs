deef05fb9d909b376382335c90dbf99e9030231f
stefanie-cyber <stefaniejvv80@gmail.com>
2026-04-28 13:36:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_A_Q2_ATM_ConsoleApplication
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("==== CTU SIMPLE ATM SYSTEM ====");
+            Console.WriteLine();
+
+               Console.WriteLine("HI! What is your name?");
+                string name = Console.ReadLine();
+
+            Console.WriteLine();
+                Console.WriteLine("WELCOME " + name.ToUpper() + "!");
+
+               
+              double balance;
+                Console.Write("Enter account balance: ");
+               while (!double.TryParse(Console.ReadLine(), out balance))
+
+                {
+                   Console.Write("Invalid input. Enter account balance: ");
+                }
+
+             
+                double withdrawal;
+              Console.Write("Enter withdrawal amount: ");
+                 while (!double.TryParse(Console.ReadLine(), out withdrawal))
+
+
+                {
+
+              Console.Write("Invalid input. Enter withdrawal amount: ");
+
+              }
+
+            Console.WriteLine();
+
+              
+                if (withdrawal <= balance)
+
+         {
+               balance = balance - withdrawal;
+
+               Console.WriteLine("Withdrawal successful!");
+                   Console.WriteLine("Updated Balance: " + balance.ToString ("0.00").Replace ('.',','));
+                    Console.WriteLine("Transaction Time: " +
+                        DateTime.Now.ToString("dd MMM yyyy HH:mm:ss"));
+
+                }
+
+                else
+
+                {
+
+                 Console.WriteLine("Insufficient funds!");
+                    Console.WriteLine("Transaction cancelled.");
+
+                }
+
+               Console.ReadKey();
+            }
+        }
+    }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Section A Q2 ATM ConsoleApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section A Q2 ATM ConsoleApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("959ae26e-74c2-4b90-a4aa-bd8e556b2f0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
